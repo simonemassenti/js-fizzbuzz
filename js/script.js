58a0c65345fe5f6a821b0e1fc7c48405cc23e751
simonemassenti <s.massenti96@gmail.com>
2023-10-23 10:27:41 +0200

    update: doc and for

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 //get HTML element row
 const rowElem = document.querySelector(".row");
-//Initialization grid global variable
+//Initialize grid global variable
 let grid = "";
 
 //Start for loop
-for (let i = 0; i <= 100; i++) {
-//Initialization local variables one for the class and one for the message
+for (let i = 1; i <= 100; i++) {
+    //Initialize local variables one for the class and one for the message
     let msg = "";
     let classIn = "";
 
     //Check if i is divisible for 3  
     if (i % 3 === 0) {
-        //Writing Fizz in the classIn variable
+        //Write Fizz in the classIn variable
         classIn = "Fizz";
-        //Writing classIn in the msg variable
+        //Write classIn in the msg variable
         msg = classIn;
     }
 
     //Check if i is also divisible for 5
     if (i % 5 === 0) {
-        //Writing Buzz in the classIn variable
+        //Write Buzz in the classIn variable
         classIn += "Buzz";
-        //Writing classIn in the msg variable
+        //Write classIn in the msg variable
         msg = classIn;
     }
 
     //Check if i is not divisible for 3 and 5
     if ((i % 3 !== 0) && (i % 5 !== 0) ) {
-        //Writing i in the msg variable
+        //Write i in the msg variable
         msg = i;
     }
     
-    //Preparing the grid
+    //Prepare the grid
     grid += `<div class="ms_square border mx-2 ${classIn}">${msg}</div>`;
 }
 
-//Writing the grid in the HTML document
+//Write the grid in the HTML document
 rowElem.innerHTML = grid;
